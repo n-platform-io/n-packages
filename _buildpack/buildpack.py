a4ecb64b62a5e747baa3639f800a5c975025f101
Michał Węglowski-Hodur <michal@michal.dev>
2023-02-18 23:54:21 +0100

    [feature] Buildpack - do not ask for confirmation

--- _buildpack/buildpack.py
+++ _buildpack/buildpack.py
@@ -151,14 +151,14 @@ if args.action == "buildall":
         print(f"Building runtime dependency: {element}")
         os.chdir(repo_dir)
         os.chdir(f"packages/{element}")
-        subprocess.run(f"PKGDEST={repo_dir}/out makepkg -sr", shell=True, check=True)
+        subprocess.run(f"PKGDEST={repo_dir}/out makepkg --noconfirm -sr", shell=True, check=True)
 
 
 elif args.action == "build":
     print(f"Building runtime dependency: {args.package}")
     os.chdir(repo_dir)
     os.chdir(f"packages/{args.package}")
-    subprocess.run(f"PKGDEST={repo_dir}/out makepkg -sr", shell=True, check=True)
+    subprocess.run(f"PKGDEST={repo_dir}/out makepkg --noconfirm -sr", shell=True, check=True)
 
 elif args.action == "check":
     err = False
